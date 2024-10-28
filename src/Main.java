47f4df4d38ba10046cf162c73c416c595e8f147a
Valeria <yakovleva1600@mail.ru>
2024-10-28 21:55:01 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player("Иванов ", "Петр", "Санкт-Петербург", "434897543857");
+System.out.println(player);
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    String surname;
+    String name;
+    String address;
+    String cardNumber;
+
+    public Player(String surname, String name, String address, String cardName) {
+        this.surname = surname;
+        this.name = name;
+        this.address = address;
+        this.cardNumber = cardName;
+
+    }
+    public String toString() {
+        return "Player: " + surname + name + "," + "address: " + address + "," + "card: " + cardNumber;
+    }
+}
